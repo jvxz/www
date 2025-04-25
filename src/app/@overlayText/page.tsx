953dd667fdc111adc99f457074cc0d89a84471ed
jamie 🌨️ <jamie@wisp.bio>
2025-04-25 18:08:24 -0400

    refactor: edit overlay

--- src/app/@overlayText/page.tsx
+++ src/app/@overlayText/page.tsx
@@ -6,8 +6,9 @@ export default async function Page({ params }: { params: Promise<{ slug: string 
   return (
     <ViewTransition
 
-      name="overlay-text">
-      <div className="text-primary fixed right-0 bottom-0 z-50 -translate-y-12 font-mono text-[300px] tracking-tighter opacity-2.5 select-none [text-box:_trim-both_cap_alphabetic]">
+      name="overlay-text"
+    >
+      <div className="text-primary fixed right-0 bottom-0 z-50 -translate-y-12 font-mono text-[300px] font-medium tracking-tighter opacity-2 select-none [text-box:_trim-both_cap_alphabetic]">
         {slug ?? 'profile'}
       </div>
     </ViewTransition>
