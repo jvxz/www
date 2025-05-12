63a3d8c038f665b66855edefa85a91092ca4bafe
jamie 🌨️ <jamie@wisp.bio>
2025-05-12 15:23:58 -0400

    chore: edit next config + add ci

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 export default function Page() {
   return (
-    <div></div>
-  );
+    <div className="container mx-auto">
+      <div className="flex h-screen items-center justify-center">
+        <h1 className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, architecto itaque nostrum atque minus nemo nulla aliquid quos adipisci nesciunt possimus sint at, illum voluptatibus inventore ab aspernatur facilis hic! Minus vel saepe blanditiis aut animi ea porro expedita quia harum itaque sequi enim esse dignissimos, ex aperiam facilis quis ratione? Modi animi amet unde molestiae. Deleniti dolores id asperiores, accusamus cum rerum aspernatur ullam sit, assumenda ducimus perspiciatis veniam voluptas? Optio eligendi tempore nobis magnam, sed quo iusto, adipisci laudantium esse ab facilis nostrum fugiat cumque molestiae repudiandae labore ex iure. Sequi molestias libero voluptas dolorem perspiciatis quasi similique, in aliquam nemo itaque magni ut nulla, pariatur et praesentium beatae rem deleniti. Animi autem quasi error! Officiis recusandae voluptas, facere ab tempora perferendis minus exercitationem maxime ut eveniet, illo quis consectetur pariatur fugiat odio dolorem in expedita beatae distinctio est. Inventore ut sequi aspernatur molestias modi fugiat dolores quos culpa repellat, odit provident dolor quaerat, repellendus omnis accusamus necessitatibus id sint vitae tempora, pariatur deleniti. Ipsam quisquam accusantium laborum nulla id. Saepe, dolorem voluptatum. Quae molestias veniam sapiente aut accusantium! Vitae animi quae, pariatur aliquid sint nemo ullam provident, necessitatibus fugit neque temporibus, itaque molestias. Maxime architecto minima consequuntur!</h1>
+      </div>
+    </div>
+  )
 }
