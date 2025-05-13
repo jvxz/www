37d44c3e7045fe41646b84d86a8f7f1f59343d8b
jamie 🌨️ <jamie@wisp.bio>
2025-05-12 20:07:49 -0400

    feat: uses content

--- src/app/uses/page.tsx
+++ src/app/uses/page.tsx
@@ -1,7 +1,83 @@
+import { Icon } from '@/components/ui/icon'
+
 export default function Page() {
   return (
-    <div>
-      <h1>Uses</h1>
-    </div>
+    <article className="prose space-y-16">
+      <section>
+        <h1>hardware</h1>
+        <ul>
+          <li>
+            <span className="iconify icon-[mingcute--laptop-line]" />
+            2021 MacBook Pro 14"
+          </li>
+          <li>
+            <span className="iconify icon-[mingcute--mouse-line]" />
+            Lemokey G2 8K
+          </li>
+          <li>
+            <span className="iconify icon-[mingcute--keyboard-line]" />
+            Lucky 65% V2
+          </li>
+          <li>
+            <span className="iconify icon-[mingcute--monitor-line]" />
+            Acer Nitro XV275U 27" 1440p
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h1>stack</h1>
+        <ul>
+          <li>
+            <Icon.TypeScript />
+            TypeScript
+          </li>
+          <li>
+            <Icon.React />
+            React
+          </li>
+          <li>
+            <Icon.Nextjs />
+            Next.js
+          </li>
+          <li>
+            <Icon.TailwindCSS />
+            Tailwind
+          </li>
+          <li>
+            <Icon.TRPC />
+            tRPC
+          </li>
+          <li>
+            <Icon.ReactQuery />
+            React Query
+          </li>
+          <li>
+            <Icon.Effect />
+            Effect
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h1>environment</h1>
+        <ul>
+          <li>
+            <span className="iconify icon-[mingcute--globe-2-line]" />
+            Arc
+          </li>
+          <li>
+            <span className="iconify icon-[mingcute--code-line]" />
+            Cursor
+          </li>
+          <li>
+            <span className="iconify icon-[mingcute--terminal-line]" />
+            Ghostty
+          </li>
+          <li>
+            <span className="iconify icon-[mingcute--package-2-line]" />
+            bun / pnpm
+          </li>
+        </ul>
+      </section>
+    </article>
   )
 }
