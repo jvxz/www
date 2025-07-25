8a63ac4c5578865d21308d796bc6c295226f09f3
jamie 🌨️ <jamie@wisp.bio>
2025-07-25 16:27:20 -0400

    feat: index page

---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col gap-6">
+    <p>Hi there! I'm a 21-year-old <span class="font-medium text-blue-400">TypeScript</span> developer from northern Georgia.</p>
+    <p>
+      I am currently working full-time on personal OSS projects, mostly specializing in full-stack web applications. I have a strong passion of discovering new technologies and concepts, specifically those that enhance quality of life. You can find my work <NuxtLink to="/work">
+        <ULink>here</ULink>
+      </NuxtLink>!
+    </p>
+    <p>
+      If you'd like to get in touch, feel free to contact me via email! <NuxtLink to="mailto:hi@jamie.to">
+        <ULink>hi@jamie.to</ULink>
+      </NuxtLink>
+    </p>
+    <p>Thanks for stopping by! ✨</p>
+  </div>
+</template>
