972f4dcdfe876ab7b3933c14614362d536a9c46b
jamie 🌨️ <jamie@wisp.bio>
2025-07-25 17:27:25 -0400

    feat: work/project card component

---
+++ app/components/work-card.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="flex flex-col gap-2">
+    <div class="flex items-center justify-between">
+      <h1 class="font-mono text-2xl font-medium">
+        <slot name="title" />
+      </h1>
+      <slot name="links" />
+    </div>
+    <p class="text-sm text-muted-foreground">
+      <slot name="subtitle" />
+    </p>
+    <div class="flex items-center justify-between">
+      <p>
+        <slot name="description" />
+      </p>
+      <div class="flex items-center gap-2 text-primary *:size-6 pr-1">
+        <slot name="stack" />
+      </div>
+    </div>
+  </section>
+</template>
