5fd103631a47e4df537aabfb8c61852dffdb9dbf
jamie 🌨️ <jamie@wisp.bio>
2025-07-25 16:26:54 -0400

    feat: button component

---
+++ app/components/u/button.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import type { VariantProps } from 'tailwind-variants';
+import { tv } from 'tailwind-variants';
+
+const props = withDefaults(defineProps<{
+  size?: VariantProps<typeof button>['size']
+  variant?: VariantProps<typeof button>['variant']
+}>(), {
+  size: 'default',
+  variant: 'default',
+})
+
+const button = tv({
+  base: 'h-8 cursor-pointer px-1',
+  variants: {
+    size: {
+      default: 'h-8',
+      icon: 'size-8',
+    },
+    variant: {
+      active: 'bg-primary text-background',
+      default: 'bg-transparent text-primary hover:bg-primary hover:text-background',
+    },
+  },
+})
+</script>
+
+<template>
+  <button
+    :size="props.size"
+    :class="button({
+      size:
+        props.size,
+      variant:
+        props.variant,
+    })"
+  >
+    <slot />
+  </button>
+</template>
