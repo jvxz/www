90c53d1b01519bad6913e7cedcf972af9d789826
jamie 🌨️ <jamie@wisp.bio>
2025-07-25 23:22:32 -0400

    feat: add progress ui component

---
+++ app/components/u/progress.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+defineProps<{
+  percentage: number
+}>()
+</script>
+
+<template>
+  <div class="relative h-3 w-full rounded-full bg-muted">
+    <div class="absolute top-0 left-0 h-full rounded-full bg-primary transition-all duration-150" :style="{ width: `${percentage}%` }" />
+  </div>
+</template>
