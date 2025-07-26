0b62ef2ffbe7679729e9fe11716fda821745109f
jamie 🌨️ <jamie@wisp.bio>
2025-07-25 21:17:08 -0400

    refactor: remove app config file

--- app/app.config.ts
+++
@@ -1,7 +0,0 @@
-export default defineAppConfig({
-  icon: {
-    customize: (content: string, _name: string, _prefix: string, _provider: string) => {
-      return content.replace(/fill="[^"]*"/g, `fill="currentColor"`)
-    },
-  },
-})
