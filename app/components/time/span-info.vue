2cb3b4c0024b1add491d91876ada7b1721f80b5a
jamie 🌨️ <jamie@wisp.bio>
2025-07-25 23:48:17 -0400

    feat: add wip loading state to time page (users should not even see a loading state anyways because of swr)

--- app/components/time/span-info.vue
+++ app/components/time/span-info.vue
@@ -13,7 +13,7 @@ const start = computed(() => {
 
 const total = computed(() => {
   if (!data.value) {
-    return '...'
+    return null
   }
   return data.value.human_readable_total.replace(' hrs', 'h').replace(' mins', 'm')
 })
@@ -25,9 +25,10 @@ const total = computed(() => {
       <p class="text-sm font-normal text-muted-foreground">
         total <span class="text-xs">(from {{ start }})</span>
       </p>
-      <h2 class="text-2xl font-medium">
+      <h2 v-if="data" class="text-2xl font-medium">
         {{ total }}
       </h2>
+      <USkeleton v-else class="h-[43.14px] w-48" />
     </div>
     <div class="flex flex-col gap-4 font-mono">
       <p class="text-sm font-normal text-muted-foreground">
@@ -39,12 +40,14 @@ const total = computed(() => {
         class="flex flex-col gap-2"
       >
         <div class="flex justify-between">
-          <p class="text-sm font-medium">
+          <p v-if="data" class="text-sm font-medium">
             {{ language.name }}
           </p>
-          <p class="text-sm">
+          <USkeleton v-else class="h-[23.77px] w-16" />
+          <p v-if="data" class="text-sm">
             {{ language.percent }}%
           </p>
+          <USkeleton v-else class="h-[23.77px] w-16" />
         </div>
         <UProgress :percentage="language.percent" />
       </div>
