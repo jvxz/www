2ef96fc2e0908a08c8f4a109c8d585f57e8d0b18
jamie 🌨️ <jamie@wisp.bio>
2025-07-25 23:23:20 -0400

    feat: add time span info component

---
+++ app/components/time/span-info.vue
@@ -0,0 +1,73 @@
+<script lang="ts" setup>
+const { span } = useTimeSpanQuery()
+
+const { data } = useFetch('/api/get-time', {
+  query: {
+    span,
+  },
+})
+
+const start = computed(() => {
+  return useDateFormat(data.value?.start ?? '', 'MMM DD, YYYY')
+})
+
+const total = computed(() => {
+  if (!data.value) {
+    return '...'
+  }
+  return data.value.human_readable_total.replace(' hrs', 'h').replace(' mins', 'm')
+})
+</script>
+
+<template>
+  <div class="flex flex-col gap-6">
+    <div class="flex flex-col gap-4 font-mono">
+      <p class="text-sm font-normal text-muted-foreground">
+        total <span class="text-xs">(from {{ start }})</span>
+      </p>
+      <h2 class="text-2xl font-medium">
+        {{ total }}
+      </h2>
+    </div>
+    <div class="flex flex-col gap-4 font-mono">
+      <p class="text-sm font-normal text-muted-foreground">
+        languages
+      </p>
+      <div
+        v-for="(language, index) in data?.languages.slice(0, 4)"
+        :key="index"
+        class="flex flex-col gap-2"
+      >
+        <div class="flex justify-between">
+          <p class="text-sm font-medium">
+            {{ language.name }}
+          </p>
+          <p class="text-sm">
+            {{ language.percent }}%
+          </p>
+        </div>
+        <UProgress :percentage="language.percent" />
+      </div>
+    </div>
+    <div class="flex flex-col gap-4 font-mono">
+      <p class="text-sm font-normal text-muted-foreground">
+        projects
+      </p>
+      <div
+        v-for="(project, index) in data?.projects.slice(0, 4)"
+        :key="index"
+        class="flex flex-col gap-2"
+      >
+        <div class="flex justify-between">
+          <p class="text-sm font-medium">
+            {{ project.name }}
+          </p>
+          <p class="text-sm">
+            {{ project.percent }}%
+          </p>
+        </div>
+        <UProgress :percentage="project.percent" />
+      </div>
+    </div>
+  </div>
+</template>
