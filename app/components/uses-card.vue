9612ed96c59fb843555cd1bf6723711e197ea7c1
jamie 🌨️ <jamie@wisp.bio>
2025-07-25 21:17:18 -0400

    feat: /uses card component

---
+++ app/components/uses-card.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col gap-6">
+    <h1 class="font-mono text-2xl font-medium">
+      <slot name="title" />
+    </h1>
+    <ul class="ml-4 flex flex-col gap-2 [&>li]:flex [&>li]:items-center [&>li]:gap-2 [&>li>span]:size-5 [&>li>span]:text-primary [&>li>span]:-translate-y-[1.5px]">
+      <slot name="item" />
+    </ul>
+  </div>
+</template>
