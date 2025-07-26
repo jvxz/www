18b70473e35aafb2528720e136da71b1a9d0a213
jamie 🌨️ <jamie@wisp.bio>
2025-07-25 23:23:13 -0400

    feat: add time span button component

---
+++ app/components/time/span-button.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  span: TimeSpan
+}>()
+
+const { span: querySpan } = useTimeSpanQuery()
+
+function formattedSpan(span: TimeSpan) {
+  switch (props.span) {
+    case 'today':
+      return 'Today'
+    case 'yesterday':
+      return 'Yesterday'
+    case '7_days':
+      return '7 days'
+    case '30_days':
+      return '30 days'
+    case 'all_time':
+      return 'All time'
+    default:
+      return span
+  }
+}
+</script>
+
+<template>
+  <UButton :variant="querySpan === props.span ? 'active' : 'default'" @click="querySpan = props.span">
+    {{ formattedSpan(props.span) }}
+  </UButton>
+</template>
