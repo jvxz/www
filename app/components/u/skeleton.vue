7a48e386f7cf067c58d8b582aae3ea1e31f2d6ff
jamie 🌨️ <jamie@wisp.bio>
2025-07-25 23:48:14 -0400

    feat: skeleton ui component

---
+++ app/components/u/skeleton.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { cx } from 'cva';
+
+const props = defineProps<{
+  class?: string
+}>()
+
+const classes = cx('animate-pulse rounded-full bg-muted', props.class)
+</script>
+
+<template>
+  <div :class="classes" />
+</template>
