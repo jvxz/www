4998c1d6c60a9fee98cee48880b40d582e37d449
jamie 🌨️ <jamie@wisp.bio>
2025-07-27 14:21:18 -0400

    fix: fix overflow issue caused by background

--- app/components/background.vue
+++ app/components/background.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pointer-events-none absolute inset-0 -z-10 h-screen w-screen overflow-hidden motion-reduce:hidden">
+  <div class="pointer-events-none absolute inset-0 -z-10 overflow-hidden motion-reduce:hidden">
     <BackgroundDot v-for="i in 8" :key="i" />
   </div>
 </template>
