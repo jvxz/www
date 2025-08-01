921b155aa061a15eb2b462dc345106878591606b
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-08-01 01:39:17 -0400

    fix: fix positioning of background

--- app/components/background.vue
+++ app/components/background.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pointer-events-none absolute inset-0 -z-10 overflow-hidden motion-reduce:hidden">
+  <div class="pointer-events-none fixed inset-0 -z-10 overflow-hidden motion-reduce:hidden">
     <BackgroundDot v-for="i in 8" :key="i" />
   </div>
 </template>
