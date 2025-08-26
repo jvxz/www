6c372824392e03a7893c548da8f1344c39c36c06
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-08-26 17:37:46 -0400

    fix: fix ui bug with progress component

--- app/components/u/progress.vue
+++ app/components/u/progress.vue
@@ -5,7 +5,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="relative h-3 w-full rounded-full bg-primary/25">
+  <div class="relative h-3 w-full rounded-full bg-primary/25 overflow-hidden">
     <div class="absolute top-0 left-0 h-full rounded-full bg-primary transition-all duration-150" :style="{ width: `${percentage}%` }" />
   </div>
 </template>
