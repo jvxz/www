2604534ce7f0527c72a731434e8cf82bf456743c
jamie <hi@jamie.to>
2026-01-16 17:56:30 -0500

    fix: fix hydration by wrapping Background component in ClientOnly

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,6 +2,8 @@
   <div data-slot="layout" class="mx-auto max-w-3xl space-y-16 px-4">
     <AppHeader />
     <slot />
-    <Background />
+    <ClientOnly>
+      <Background />
+    </ClientOnly>
   </div>
 </template>
